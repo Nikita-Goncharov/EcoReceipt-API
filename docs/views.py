4eff7e1c8d681a8d89f1b0ac2ff395d82d27920f
Nikita Goncharov <nikitagoncarov657@gmail.com>
2024-09-30 20:23:15 +0300

    add endpoints docs like the main page

---
+++ docs/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+
+def docs(request: HttpRequest) -> HttpResponse:
+    return render(request, "docs.html")
--- ecoreceipt_api/urls.py
+++ ecoreceipt_api/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from docs import views
+
 urlpatterns = [
+    path("", views.docs),
     path('admin/', admin.site.urls),
     path("terminal_api/", include("terminal_api.urls")),
     path("client_api/", include("client_api.urls"))
